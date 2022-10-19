1ea1fa3500d98b8fc33478f97f056b8cfc03eeec
Yevgeniy <96199833+ytolks@users.noreply.github.com>
2022-10-19 13:28:18 +0200

    Update DemoApplication.java
    add test the conflict

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("My first spring application");
 		System.out.println("Cool start");
+		System.out.println("Cool start for sure");
 
 	}
 
