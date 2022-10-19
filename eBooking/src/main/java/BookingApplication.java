3344580787c16fbfb02512f1ace451295e8ebde0
Yevgeniy <96199833+ytolks@users.noreply.github.com>
2022-10-19 23:19:36 +0200

    Update BookingApplication.java
    updating...

--- eBooking/src/main/java/BookingApplication.java
+++ eBooking/src/main/java/BookingApplication.java
@@ -3,6 +3,7 @@ public class BookingApplication {
     public static void main(String[] args) {
         System.out.println("My first application steps");
         System.out.println("My second step");
+        System.out.println("My third step");
 
     }
 }
