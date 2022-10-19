280ba5fac80153d8d9ce14c0af331f4b3c56d9b3
Yevgeniy <yevgeniy.tolks@gmail.com>
2022-10-19 23:20:12 +0300

    2nd commit

--- eBooking/src/main/java/BookingApplication.java
+++ eBooking/src/main/java/BookingApplication.java
@@ -2,5 +2,7 @@
 public class BookingApplication {
     public static void main(String[] args) {
         System.out.println("My first application steps");
+        System.out.println("My second step");
+
     }
 }
