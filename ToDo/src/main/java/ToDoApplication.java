7e579247e781393a582a75731f2c98341508c790
Yevgeniy <yevgeniy.tolks@gmail.com>
2022-10-19 23:08:00 +0300

    first message

---
+++ ToDo/src/main/java/ToDoApplication.java
@@ -0,0 +1,5 @@
+public class ToDoApplication {
+    public static void main(String[] args) {
+        System.out.println("My first application");
+    }
+}
---
+++ eBooking/src/main/java/BookingApplication.java
@@ -0,0 +1,6 @@
+
+public class BookingApplication {
+    public static void main(String[] args) {
+        System.out.println("My first application steps");
+    }
+}
